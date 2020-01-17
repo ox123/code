1bcfca81f2703edd8badb6e5bf2e18533d40de6f
ouxu <gyouxu@gmail.com>
2020-01-18 01:03:37 +0800

    update msg

--- src/main/java/org/code/linklist/MergeLinkList_21.java
+++ src/main/java/org/code/linklist/MergeLinkList_21.java
@@ -5,27 +5,40 @@ public class MergeLinkList_21 {
 
     }
 
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+     * Memory Usage: 41.6 MB, less than 7.07% of Java online submissions for Merge Two Sorted Lists.
+     * TODO 内存有待优化
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return null;
-        if (l1 == null && l2 != null) return l2;
-        if (l1 != null && l2 == null) return l1;
+        if (l1 == null && l2 == null) {
+            return null;
+        } else if (l1 == null && l2 != null) {
+            return l2;
+        } else if (l1 != null && l2 == null) {
+            return l1;
+        }
         ListNode head = new ListNode(0);
         ListNode res = head;
         while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                head.next = l1;
+            if (l1.val <= l2.val) { // =号用的比较好
+                res.next = l1;
                 l1 = l1.next;
-            } else if (l1.val > l2.val){
-                head.next = l2;
-                l2 = l2.next;
-            }else {
-                head.next = l1;
-                l1 = l1.next;
+            } else {
+                res.next = l2;
                 l2 = l2.next;
             }
-            head = head.next;
+            res = res.next; // 移动指针
         }
-        res.next = l1.next != null ? l1 : l2;
-        return res.next;
+        if (l1 != null) {
+            res.next = l1;
+        }
+        if (l2 != null) {
+            res.next = l2;
+        }
+        return head.next;
     }
 }
