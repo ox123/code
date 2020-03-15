bbd64cdca1e0c36ce75f8137e519705549d4ef6d
ouxu <gyouxu@gmail.com>
2020-03-15 19:45:08 +0100

    anagrames

---
+++ src/main/java/org/code/dynamic/MergeStones_1000.java
@@ -0,0 +1,15 @@
+package org.code.dynamic;
+
+/**
+ * https://leetcode.com/problems/minimum-cost-to-merge-stones/
+ */
+public class MergeStones_1000 {
+    public static void main(String[] args) {
+
+    }
+
+    public int mergeStones(int[] stones, int K) {
+        int result = 0;
+        return result;
+    }
+}
---
+++ src/main/java/org/code/str/IsAnagram_242.java
@@ -0,0 +1,39 @@
+package org.code.str;
+
+import java.util.Arrays;
+
+public class IsAnagram_242 {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(new IsAnagram_242().isAnagram(s, t));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        char[] chars1 = s.toCharArray();
+        char[] chars = t.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars);
+        return String.valueOf(chars).equals(String.valueOf(chars1));
+    }
+
+    public boolean isAnagram1(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        int arr[] = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for (int counter :
+                arr) {
+            if (counter != 0) return false;
+        }
+        return true;
+    }
+}
