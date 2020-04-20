02bc245eb24e355ad374e0cb48bdccb1738151e6
ouxu <gyouxu@gmail.com>
2020-04-20 20:33:35 +0200

    19. Remove Nth Node From End of List---slow,fast pointer

--- src/main/java/org/leetcode/linklist/RemoveNthFromEnd_19.java
+++ src/main/java/org/leetcode/linklist/RemoveNthFromEnd_19.java
@@ -39,6 +39,30 @@ public class RemoveNthFromEnd_19 {
         return head;
     }
 
+    /**
+     * 快慢指针
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+
+        ListNode slow = temp;
+        ListNode fast = temp;
+
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return temp.next;
+    }
 
     /**
      * fail method
