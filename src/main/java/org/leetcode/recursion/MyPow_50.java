9fd80a4ee8b2ae1b343a797545a6634b92946e45
gyouxu <gyouxu@gmail.com>
2020-07-13 00:19:09 +0200

    resolved 78

---
+++ src/main/java/org/leetcode/recursion/MyPow_50.java
@@ -0,0 +1,7 @@
+package org.leetcode.recursion;
+
+public class MyPow_50 {
+    public double myPow(double x, int n) {
+        return 0.0f;
+    }
+}
--- src/main/java/org/leetcode/stackqueue/Subsets_78.java
+++ src/main/java/org/leetcode/stackqueue/Subsets_78.java
@@ -14,7 +14,21 @@ public class Subsets_78 {
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null) return ans;
+        dfs(ans, nums, new ArrayList<>(), 0);
+        return ans;
+    }
+
+    private void dfs(List<List<Integer>> ans, int[] nums, ArrayList<Integer> list, int index) {
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        dfs(ans, nums, list, index + 1); // not pick the number at this index
+        list.add(nums[index]);
+        dfs(ans, nums, list, index + 1); // pick the number at this index
+        // reverse  the current state
+        list.remove(list.size() - 1);
     }
 }
