ef3d42b0e2452e6a8d6cbd832dde6f181f6ebb20
ouxu <gyouxu@gmail.com>
2020-08-11 20:37:29 +0200

    resolved 100 other ways

--- src/main/java/org/leetcode/tree/SameTree_100.java
+++ src/main/java/org/leetcode/tree/SameTree_100.java
@@ -3,14 +3,25 @@ package org.leetcode.tree;
 public class SameTree_100 {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == q;
-        }
-
-        if (p.val != q.val) {
+//        if (p == null || q == null) {
+//            return p == q;
+//        }
+//
+//        if (p.val != q.val) {
+//            return false;
+//        }
+//        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
             return false;
         }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
     }
 
     public class TreeNode {
