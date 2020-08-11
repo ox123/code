088afaa53cc8f8626c54d2af68f4aefc6404b502
ouxu <gyouxu@gmail.com>
2020-08-11 20:32:03 +0200

    resolved 100

---
+++ src/main/java/org/leetcode/tree/SameTree_100.java
@@ -0,0 +1,36 @@
+package org.leetcode.tree;
+
+public class SameTree_100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
