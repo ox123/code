df72619da15726aab71ba5c083a4c136056cd4aa
gyouxu <gyouxu@gmail.com>
2020-08-23 22:00:33 +0200

    resolved 50

--- src/main/java/org/leetcode/recursion/MyPow_50.java
+++ src/main/java/org/leetcode/recursion/MyPow_50.java
@@ -1,7 +1,27 @@
 package org.leetcode.recursion;
 
+/**
+ * 1. 分治
+ * 2. 牛顿迭代法
+ */
 public class MyPow_50 {
-    public double myPow(double x, int n) {
-        return 0.0f;
+    public static void main(String[] args) {
+        double x = 2.0;
+        int n = 10;
+        System.out.println(new MyPow_50().myPow(x, n));
+//        Math.pow()
     }
+
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else if (n < 0) {
+            return (1 / x) * half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
 }
