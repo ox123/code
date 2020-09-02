549f22667fb356b7634cf5adcdf99f805b05558e
gyouxu <gyouxu@gmail.com>
2020-09-02 22:46:33 +0200

    resolved 402

---
+++ src/main/java/org/leetcode/stackqueue/RemoveKdigits_402.java
@@ -0,0 +1,35 @@
+package org.leetcode.stackqueue;
+
+import java.util.ArrayDeque;
+
+public class RemoveKdigits_402 {
+    public static void main(String[] args) {
+        String num = "1234567890";
+        int n = 9;
+        System.out.println(new RemoveKdigits_402().removeKdigits(num, n));
+    }
+
+    public String removeKdigits(String num, int k) {
+        ArrayDeque<Character> dp = new ArrayDeque<Character>();
+        for (char c :
+                num.toCharArray()) {
+            while (!dp.isEmpty() && c < dp.getLast() & k != 0) {
+                dp.removeLast();
+                k--;
+            }
+            dp.addLast(c);
+        }
+        while (k != 0) {
+            dp.removeLast();
+            k--;
+        }
+        while (!dp.isEmpty() && dp.getFirst() == '0') {
+            dp.removeFirst();
+        }
+        StringBuilder builder = new StringBuilder();
+        while (!dp.isEmpty()) {
+            builder.append(dp.pollFirst());
+        }
+        return builder.length() == 0 ? "0" : builder.toString();
+    }
+}
