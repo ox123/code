a5b3fb254f11a401c4346dbd2ba855b3f8be32b8
gyouxu <gyouxu@gmail.com>
2020-10-14 22:44:14 +0200

    optimized 46

--- src/main/java/org/leetcode/recursion/Permutations_46.java
+++ src/main/java/org/leetcode/recursion/Permutations_46.java
@@ -32,4 +32,34 @@ public class Permutations_46 {
             used[i] = false;
         }
     }
+
+    /**
+     * 另外一种思路
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        if (nums.length == 0) {
+            return permutations;
+        }
+
+        collectPermutations(nums, 0, new ArrayList<>(), permutations);
+        return permutations;
+    }
+
+    private void collectPermutations(int[] nums, int start, List<Integer> permutation,
+                                     List<List<Integer>>  permutations) {
+
+        if (permutation.size() == nums.length) {
+            permutations.add(permutation);
+            return;
+        }
+
+        for (int i = 0; i <= permutation.size(); i++) {
+            List<Integer> newPermutation = new ArrayList<>(permutation);
+            newPermutation.add(i, nums[start]);
+            collectPermutations(nums, start + 1, newPermutation, permutations);
+        }
+    }
 }
